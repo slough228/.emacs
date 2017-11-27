9b49ae44e0467aef1ccb321ea0f98b56cb0b362c
Samuel Lough <samuel.lough@gmail.com>
2017-11-27 12:03:40 -0500

    working with elpy, ggtags, themes. pick autocompletion engine still needed.

---
+++ elpa/anti-zenburn-theme-20160725.1559/anti-zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "anti-zenburn-theme" "20160725.1559" "Low-contrast Zenburn-inverted theme" 'nil :commit "164122ebb7a39c41b953e90fe05cf440c5335d9e" :url "https://github.com/m00natic/anti-zenburn-theme")
---
+++ elpa/cmake-mode-20160928.505/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20160928.505" "major-mode for editing CMake sources" 'nil :commit "14ffd3b7e9377e8a2ea4a187ebe6517f20721ed8")
